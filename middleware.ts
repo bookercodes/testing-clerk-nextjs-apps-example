55a5d38040e3611656fc48a4cebf7c4a17a6912f
Lily Barrett <lilybarrett@Lilys-MacBook-Air-2021.local>
2025-03-20 22:41:26 -0400

    Remove some unnecessary files

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/submit-review"]);
+const isProtectedRoute = createRouteMatcher(["/submit-review"]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect();
