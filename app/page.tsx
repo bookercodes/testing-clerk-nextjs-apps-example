4c0ac6a4dee26c60fd5dbaacd1a43364c74a44e1
Lily Barrett <lilybarrett@Lilys-MacBook-Air-2021.local>
2025-03-24 13:08:05 -0400

    More changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
-import { SignInButton, SignOutButton, SignedIn, SignedOut } from "@clerk/nextjs"
-import dogAtRestaurant from "./images/dog_at_restaurant.jpg"
-import "./home.css"
-import Image from "next/image"
-import Link from "next/link"
+import { AuthActions } from "./components/auth-actions";
+import dogAtRestaurant from "./images/dog_at_restaurant.jpg";
+import "./home.css";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -17,30 +16,7 @@ export default function Home() {
             <p className="text-[#5E5F6E] pt-3 pb-6 text-[1.0625rem] relative">
               An application for dog owners to find dog-friendly restaurants, cafes, and more
             </p>
-            <div className="relative flex gap-3">
-              <SignedIn>
-                <Link
-                  href="/submit-review"
-                  className="px-4 py-2 rounded-full bg-[#131316] text-white text-sm font-semibold"
-                >
-                 Submit a review
-                </Link>
-                <SignOutButton>
-                  <button className="px-4 py-2 rounded-full bg-[#131316] text-white text-sm font-semibold">
-                    Sign out
-                  </button>
-                </SignOutButton>
-              </SignedIn>
-            </div>
-            <div>
-              <SignedOut>
-                <SignInButton>
-                  <button className="px-4 py-2 rounded-full bg-[#131316] text-white text-sm font-semibold">
-                    Sign in
-                  </button>
-                </SignInButton>
-               </SignedOut>
-            </div>
+            <AuthActions />
           </div>
           <div className="flex gap-8">
             <div>
