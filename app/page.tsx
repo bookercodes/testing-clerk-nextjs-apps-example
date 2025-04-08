3bcc536d38f6596664ae3663e80bbe69ceb6d284
Alex Booker <booker@booker.codes>
2025-04-08 15:09:19 +0100

    remove dog image

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
-import { AuthActions } from "./components/auth-actions";
-import dogAtRestaurant from "./images/dog_at_restaurant.jpg";
-import "./home.css";
-import Image from "next/image";
+import { AuthActions } from "./components/auth-actions"
+import "./home.css"
+import Image from "next/image"
 
 export default function Home() {
   return (
@@ -14,19 +13,10 @@ export default function Home() {
             </h1>
 
             <p className="text-[#5E5F6E] pt-3 pb-6 text-[1.0625rem] relative">
-              An application for dog owners to find dog-friendly restaurants, cafes, and more
+              An application for dog owners to find dog-friendly restaurants,
+              cafes, and more
             </p>
             <AuthActions />
-          </div>
-          <div className="flex gap-8">
-            <div>
-              <Image
-                alt="Device"
-                src={dogAtRestaurant}
-                unoptimized
-                className="flex-none rounded-xl bg-white shadow-[0_5px_15px_rgba(0,0,0,0.08),0_15px_35px_-5px_rgba(25,28,33,0.2)] ring-1 ring-gray-950/5"
-              />
-            </div>
           </div>
         </div>
       </main>
